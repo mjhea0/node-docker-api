152a5954f8194c63bb77b0fd1f94a2f4813082ba
Michael Herman <hermanmu@gmail.com>
2017-04-06 12:40:19 -0600

    updated location tests

--- services/locations/tests/integration/routes.locations.test.js
+++ services/locations/tests/integration/routes.locations.test.js
@@ -22,21 +22,31 @@ describe('Locations API Routes', () => {
   });
 
   describe('GET /locations', () => {
-    // it('should return all locations', (done) => {
-    //   chai.request(server)
-    //   .get('/locations')
-    //   .end((err, res) => {
-    //     res.type.should.equal('application/json');
-    //     res.body.status.should.equal('success');
-    //     res.body.data.should.be.a('array');
-    //     res.body.data.length.should.equal(1);
-    //     res.body.data[0].should.have.property('user_id');
-    //     res.body.data[0].should.have.property('lat');
-    //     res.body.data[0].should.have.property('long');
-    //     res.body.data[0].should.have.property('created_at');
-    //     done();
-    //   });
-    // });
+    it('should return all locations', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        chai.request(server)
+        .get('/locations')
+        .set('authorization', `Bearer ${response.body.token}`)
+        .end((err, res) => {
+          res.type.should.equal('application/json');
+          res.body.status.should.equal('success');
+          res.body.data.should.be.a('array');
+          res.body.data.length.should.equal(1);
+          res.body.data[0].should.have.property('user_id');
+          res.body.data[0].should.have.property('lat');
+          res.body.data[0].should.have.property('long');
+          res.body.data[0].should.have.property('created_at');
+          done();
+        });
+      });
+    });
     it('should throw an error if a user is not logged in', (done) => {
       chai.request(server)
       .get('/locations')
@@ -50,29 +60,39 @@ describe('Locations API Routes', () => {
     });
   });
   describe('GET /locations/:id', () => {
-    // it('should return a single location', () => {
-    //   return queries.getAllLocations()
-    //   .then((locations) => {
-    //     const location = locations[0];
-    //     chai.request(server)
-    //     .get(`/locations/${location.id}`)
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.type.should.equal('application/json');
-    //       res.body.status.should.equal('success');
-    //       res.body.data.should.be.a('object');
-    //       res.body.data.should.have.property('id');
-    //       res.body.data.id.should.equal(location.id);
-    //       res.body.data.should.have.property('user_id');
-    //       res.body.data.user_id.should.equal(location.user_id);
-    //       res.body.data.should.have.property('lat');
-    //       res.body.data.lat.should.equal(location.lat);
-    //       res.body.data.should.have.property('long');
-    //       res.body.data.long.should.equal(location.long);
-    //       res.body.data.should.have.property('created_at');
-    //     });
-    //   });
-    // });
+    it('should return a single location', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        return queries.getAllLocations()
+        .then((locations) => {
+          const location = locations[0];
+          chai.request(server)
+          .get(`/locations/${location.id}`)
+          .set('authorization', `Bearer ${response.body.token}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.type.should.equal('application/json');
+            res.body.status.should.equal('success');
+            res.body.data.should.be.a('object');
+            res.body.data.should.have.property('id');
+            res.body.data.id.should.equal(location.id);
+            res.body.data.should.have.property('user_id');
+            res.body.data.user_id.should.equal(location.user_id);
+            res.body.data.should.have.property('lat');
+            res.body.data.lat.should.equal(location.lat);
+            res.body.data.should.have.property('long');
+            res.body.data.long.should.equal(location.long);
+            res.body.data.should.have.property('created_at');
+          });
+        });
+      });
+    });
     it('should throw an error if a user is not logged in', () => {
       return queries.getAllLocations()
       .then((locations) => {
@@ -89,51 +109,71 @@ describe('Locations API Routes', () => {
     });
   });
   describe('POST /', () => {
-    // it('should create a new location', () => {
-    //   return queries.getAllLocations()
-    //   .then((locationsBefore) => {
-    //     chai.request(server)
-    //     .post('/locations')
-    //     .send({
-    //       user_id: 99,
-    //       lat: 46.9,
-    //       long: -47.8,
-    //     })
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.type.should.equal('application/json');
-    //       res.body.should.be.a('object');
-    //       res.body.status.should.equal('success');
-    //       res.body.data.should.equal('Location Added!');
-    //       return queries.getAllLocations()
-    //       .then((locationsAfter) => {
-    //         locationsAfter.length.should.equal(locationsBefore.length + 1);
-    //         locationsAfter[locationsAfter.length - 1].user_id.should.equal(99);
-    //       });
-    //     });
-    //   });
-    // });
-    // it('should NOT create a location missing a user_id', () => {
-    //   return queries.getAllLocations()
-    //   .then((locationsBefore) => {
-    //     chai.request(server)
-    //     .post('/locations')
-    //     .send({
-    //       lat: 77,
-    //       long: 77,
-    //     })
-    //     .end((err, res) => {
-    //       res.should.have.status(500);
-    //       res.type.should.equal('application/json');
-    //       res.body.status.should.equal('error');
-    //       return queries.getAllLocations()
-    //       .then((locationsAfter) => {
-    //         locationsAfter.length.should.equal(locationsBefore.length);
-    //         locationsAfter[locationsAfter.length - 1].lat.should.not.equal(77);
-    //       });
-    //     });
-    //   });
-    // });
+    it('should create a new location', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        return queries.getAllLocations()
+        .then((locationsBefore) => {
+          chai.request(server)
+          .post('/locations')
+          .set('authorization', `Bearer ${response.body.token}`)
+          .send({
+            user_id: 99,
+            lat: 46.9,
+            long: -47.8,
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.type.should.equal('application/json');
+            res.body.should.be.a('object');
+            res.body.status.should.equal('success');
+            res.body.data.should.equal('Location Added!');
+            return queries.getAllLocations()
+            .then((locationsAfter) => {
+              locationsAfter.length.should.equal(locationsBefore.length + 1);
+              locationsAfter[locationsAfter.length - 1].user_id.should.equal(99);
+            });
+          });
+        });
+      });
+    });
+    it('should NOT create a location missing a user_id', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        return queries.getAllLocations()
+        .then((locationsBefore) => {
+          chai.request(server)
+          .post('/locations')
+          .set('authorization', `Bearer ${response.body.token}`)
+          .send({
+            lat: 77,
+            long: 77,
+          })
+          .end((err, res) => {
+            res.should.have.status(500);
+            res.type.should.equal('application/json');
+            res.body.status.should.equal('error');
+            return queries.getAllLocations()
+            .then((locationsAfter) => {
+              locationsAfter.length.should.equal(locationsBefore.length);
+              locationsAfter[locationsAfter.length - 1].lat.should.not.equal(77);
+            });
+          });
+        });
+      });
+    });
     it('should throw an error if a user is not logged in', () => {
       return queries.getAllLocations()
       .then((locationsBefore) => {
@@ -154,32 +194,42 @@ describe('Locations API Routes', () => {
     });
   });
   describe('PUT /:id', () => {
-    // it('should update a location', () => {
-    //   return queries.getAllLocations()
-    //   .then((locationsBefore) => {
-    //     const locationID = locationsBefore[0].id;
-    //     chai.request(server)
-    //     .put(`/locations/${locationID}`)
-    //     .send({
-    //       user_id: 77,
-    //       lat: 77,
-    //       long: 77,
-    //     })
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.type.should.equal('application/json');
-    //       res.body.should.be.a('object');
-    //       res.body.status.should.equal('success');
-    //       res.body.data.should.equal('Location Updated!');
-    //       return queries.getSingleLocation(locationID)
-    //       .then((locations) => {
-    //         locations[0].user_id.should.equal(77);
-    //         locations[0].lat.should.equal(77);
-    //         locations[0].long.should.equal(77);
-    //       });
-    //     });
-    //   });
-    // });
+    it('should update a location', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        return queries.getAllLocations()
+        .then((locationsBefore) => {
+          const locationID = locationsBefore[0].id;
+          chai.request(server)
+          .put(`/locations/${locationID}`)
+          .set('authorization', `Bearer ${response.body.token}`)
+          .send({
+            user_id: 77,
+            lat: 77,
+            long: 77,
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.type.should.equal('application/json');
+            res.body.should.be.a('object');
+            res.body.status.should.equal('success');
+            res.body.data.should.equal('Location Updated!');
+            return queries.getSingleLocation(locationID)
+            .then((locations) => {
+              locations[0].user_id.should.equal(77);
+              locations[0].lat.should.equal(77);
+              locations[0].long.should.equal(77);
+            });
+          });
+        });
+      });
+    });
     it('should throw an error if a user is not logged in', () => {
       return queries.getAllLocations()
       .then((locationsBefore) => {
@@ -201,25 +251,35 @@ describe('Locations API Routes', () => {
     });
   });
   describe('DELETE /:id', () => {
-    // it('should delete a location', () => {
-    //   return queries.getAllLocations()
-    //   .then((locationsBefore) => {
-    //     const locationID = locationsBefore[0].id;
-    //     chai.request(server)
-    //     .delete(`/locations/${locationID}`)
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.type.should.equal('application/json');
-    //       res.body.should.be.a('object');
-    //       res.body.status.should.equal('success');
-    //       res.body.data.should.equal('Location Removed!');
-    //       return queries.getSingleLocation(locationID)
-    //       .then((locations) => {
-    //         locations.length.should.equal(0);
-    //       });
-    //     });
-    //   });
-    // });
+    it('should delete a location', (done) => {
+      chai.request(server)
+      .post('/users/login')
+      .send({
+        username: 'jeremy',
+        password: 'johnson123'
+      })
+      .end((error, response) => {
+        should.not.exist(error);
+        return queries.getAllLocations()
+        .then((locationsBefore) => {
+          const locationID = locationsBefore[0].id;
+          chai.request(server)
+          .delete(`/locations/${locationID}`)
+          .set('authorization', `Bearer ${response.body.token}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.type.should.equal('application/json');
+            res.body.should.be.a('object');
+            res.body.status.should.equal('success');
+            res.body.data.should.equal('Location Removed!');
+            return queries.getSingleLocation(locationID)
+            .then((locations) => {
+              locations.length.should.equal(0);
+            });
+          });
+        });
+      });
+    });
     it('should throw an error if a user is not logged in', () => {
       return queries.getAllLocations()
       .then((locationsBefore) => {
