8f8c72f14484a0477610f1143fb7958883d99e6c
Michael Herman <hermanmu@gmail.com>
2017-04-06 23:31:32 -0600

    added web

---
+++ web/src/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
+
+// require('dotenv').config();
+
+const routes = require('./routes/index');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+nunjucks.configure(path.join(__dirname, 'views'), {
+  express: app,
+  autoescape: true,
+});
+app.set('view engine', 'html');
+
+if (process.env.NODE_ENV !== 'test') { app.use(logger('dev')); }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/* eslint-disable no-unused-vars */
+app.use((err, req, res, next) => {
+  const message = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.json({
+    status: 'error',
+    message: err,
+  });
+});
+/* eslint-enable no-unused-vars */
+
+module.exports = app;
