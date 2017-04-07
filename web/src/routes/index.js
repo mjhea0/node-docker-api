372bd628fd25fefc4cd59e7ed0312bfa8e9b0197
Michael Herman <hermanmu@gmail.com>
2017-04-07 14:52:00 -0600

    updated web

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -2,7 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+/*
+- login
+- register
+- main (weather page)
+- user page (search history)
+ */
+
+function ensureAuthenticated(req, res, next) {
+  if (!(req.headers && req.headers.authorization)) {
+    res.redirect('/login');
+  }
+  // decode the token
+  const header = req.headers.authorization.split(' ');
+  const token = header[1];
+  if (!token) {
+    res.redirect('/login');
+  }
+  next();
+}
+
+router.get('/', ensureAuthenticated, (req, res) => {
   res.render('main.html');
 });
 
